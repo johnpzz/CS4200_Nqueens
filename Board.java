aaa8088caf55ac51075073cc6950ebaa04ef297f
John Zubiri <34234157+johnpzz@users.noreply.github.com>
2018-10-11 01:42:33 -0700

    Add files via upload

--- Board.java
+++ Board.java
@@ -29,6 +29,19 @@ public class Board {
         listOfQueens = new ArrayList<Point>();
     }
     
+    Board(Board b) {
+        gameBoard = new int[21][21];
+        
+        for (int i = 0; i < gameBoard.length; i++)
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                gameBoard[i][j] = b.gameBoard[i][j];
+            }
+        
+        heuristic = b.heuristic;
+        
+        listOfQueens = b.listOfQueens;
+    }
+    
     
     //One queen per column!
     public int[][] generateBoard() {
@@ -202,9 +215,7 @@ public class Board {
                     
                 }
             }
-            
-        
-            
+
             //NUM OF PAIRS = SIZE OF ALL ARRAYLISTS IN MAP
             Set<Point> mapSet = pairMap.keySet();
             for (Point p : mapSet) {
@@ -217,171 +228,33 @@ public class Board {
         return numOfPairs;
     }
     
-    /*
-    public int calculateConflictingPairs(int[][] board) {
-        int numOfPairs = 0;
-        Map<Point, ArrayList<Point>> conflictMap = new HashMap<>();
-
+    
+    public Board simulatedAnnealing(Board inputBoard) {
+        Board currentBoard = inputBoard;
+        Board nextBoard;
+        double T = 1000;
         
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 1) {
-                    
-                    // Check for unique attacking pairs (-->)
-                    //LEFT --> RIGHT conflicting pairs
-                    for (int l = j+1; l < board[i].length; l++) {
-                        if (board[i][l] == 1) {
-                            Point one = new Point(i,j);
-                            Point two = new Point(i,l);
-                            //Coordinate one = new Coordinate(i,j);
-                           // Coordinate two = new Coordinate(i,l);
-                            System.out.println("Conflict left -> right:");
-                            System.out.println("Point one: " + one.x + " " + one.y);
-                            System.out.println("Point two: " + two.x + " " + two.y);
-                            
-                            
-                            
-                            if (!conflictMap.get(one).contains(two) && !conflictMap.get(two).contains(one)) {
-                                conflictMap.get(one).add(two);
-                            }
-                            
-                            /*
-                            if (!conflictMap.containsKey(one) && !conflictMap.containsValue(two)) {
-                                conflictMap.put(one, two);
-                            }
-                            */
-
-                                    
-                            /*
-                            if (!conflictMap.get(one).equals(two)) {
-                                conflictMap.put(one, two);
-                            }
-                            */
-                            
-                            /*if (!conflictMap.containsKey(one) && !conflictMap.containsValue(two)) {
-                                conflictMap.put(one, two);
-                            }
-
-                        }
-                    }
-                    
-                    /*
-                    //LEFT <-- RIGHT conflicting pairs
-                    for (int l = j-1; l >= 0; l--) {
-                        if (board[i][l] == 1) {
-                            Point one = new Point(i,j);
-                            Point two = new Point(i,l);
-                            System.out.println("Conflict left <- right:");
-                            System.out.println("Point one: " + one.x + " " + one.y);
-                            System.out.println("Point two: " + two.x + " " + two.y);
-                                    
-                            
-                            if (!conflictMap.containsKey(one) && !conflictMap.containsValue(two)) {
-                                conflictMap.put(one, two);
-                            }
-
-                        }
-                    }
-                    
-                    
-                    //UP -> right diagonal conflicting pairs
-                    
-                    int r = j+1;
-                    for (int u = i-1; u >= 0; u--) {
-                        //for (int r = j; j < board[i].length; r++) {
-                        if (r < board[i].length) {
-                                if (board[u][r] == 1) {
-                                Point one = new Point (i,j);
-                                Point two = new Point(u,r);
-                                System.out.println("Conflict up -> right");
-                                System.out.println("Point one: " + one.x + " " + one.y);
-                                System.out.println("Point two: " + two.x + " " + two.y);
-                                
-                            // The magnum opus.
-                            if (!conflictMap.get(one).contains(two) && !conflictMap.get(two).contains(one)) {
-                                conflictMap.get(one).add(two);
-                            }
-                                
-                            }
-                                r++;
-                        }
-                            
-                        //}
-                    }
-                    
-                    //UP -> left diagonal conflicting pairs
-                    int l = j-1;
-                    for (int u = i-1; u >= 0; u--) {
-                        //for (int r = j; j < board[i].length; r++) {
-                        if (l >= 0) {
-                                if (board[u][l] == 1) {
-                                Point one = new Point (i,j);
-                                Point two = new Point(u,l);
-                                System.out.println("Conflict up -> left");
-                                System.out.println("Point one: " + one.x + " " + one.y);
-                                System.out.println("Point two: " + two.x + " " + two.y);
-                                
-                            // The magnum opus.
-                            if (!conflictMap.get(one).contains(two) && !conflictMap.get(two).contains(one)) {
-                                conflictMap.get(one).add(two);
-                            }
-                                
-                            }
-                                l--;
-                        }
-                            
-                        //}
-                    }
-                    
-                    for (int u = i-1; u >=0; u--) {
-                        int  = j-1;
-                        if (board[u][r] == 1) {
-                                Point one = new Point (i,j);
-                                Point two = new Point(u,r);
-                                System.out.println("Conflict up -> left");
-                                System.out.println("Point one: " + one.x + " " + one.y);
-                                System.out.println("Point two: " + two.x + " " + two.y);
-                            }
-                        
-                    }
-                    
-
-                    
-                    //DOWN->right diagonal conflicting pairs
-                    //DOWN->left diagonal conflicting pairs
-                    
-                    
-                    
-                    
-                    
-                }
-                
-
-
-            }
+        while (T >= 0) {
+            if (T == 0) {
+                return currentBoard;
+        }
+            T = T - .001;
+            
+            
         }
 
         
-        Set<Point> keySet = conflictMap.keySet();
-        List<ArrayList<Point>> valueList = new ArrayList<>(conflictMap.values());
         
-        System.out.println("------Key-Value Pairs-----");
-        int index = 0;
-        for (Point p : keySet) {
-            System.out.println("Key:" +index + " " + p.toString());
-            index++;
-        }
-        
-        for (ArrayList p : valueList) {
-            System.out.println("Value:" + index + " " + p.get(index));
-            index++;
-        }
-        
-        numOfPairs = conflictMap.size();
-        
-        return numOfPairs;
+        return currentBoard;
     }
-    */
+    
+    public Board generateRandomSuccessorBoard(Board inputBoard) {
+        // Randomly move one queen
+        Board successorBoard = new Board();
+        
+    }
+   
+    
     
     public static boolean equals(Point p1, Point p2) {
         if (p1.x == p2.x && p1.y == p2.y) {
@@ -415,25 +288,12 @@ public class Board {
         }
         
         b.listOfQueens = b.calculateQueens();
+        b.heuristic = b.calculateConflictingPairs();
         
         //b.heuristic = b.calculateConflictingPairs(b.gameBoard);
         System.out.println("Conflicting pairs: " + b.calculateConflictingPairs());
         
-       
-        
-        
-        /* Test
-        Pair a = new Pair(new Coordinate(1,1), new Coordinate(1,2));
-        Pair b = new Pair(new Coordinate(1,1), new Coordinate(1,2));
-        
-        if (a.first.x == b.first.x) {
-            System.out.println("equals");
-        } else {
-            System.out.println("not equals");
-        }
-        */
 
-        
         
     }
     
